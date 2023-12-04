326555b83bf9f5f8ad634e2594967b3e3cb7c596
MalinkyZubr <ahumanbeing189@gmail.com>
2023-12-04 09:33:57 -0500

    added utility

---
+++ microcontroller/libsync.sh
@@ -0,0 +1,3 @@
+# this bash script takes the "ARDUINOLIB" (should be the arduino library folder) and syncs the library to it
+
+rsync -av --delete ./IoTGreenhouseSystem $ARDUINOLIB/IoTGreenhouseSystem
